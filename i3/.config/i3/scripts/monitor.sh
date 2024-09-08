f899a089913dbf281a581dbc511f9a622c419527
sam tunder <sam@tunder@gmail.com>
2024-09-08 22:45:23 +0200

    change showing keybinds button and setup monitor for 4k

--- i3/.config/i3/scripts/monitor.sh
+++ i3/.config/i3/scripts/monitor.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output HDMI-0 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DP-0 --off --output DP-1 --off --output DP-2 --off --output DP-3 --off --output DP-4 --off
+xrandr --output DisplayPort-0 --primary --mode 3840x2160 --rate 120 --pos 0x0 --rotate normal --output DisplayPort-1 --off --output DisplayPort-2 --off --output HDMI-A-0 --off --output HDMI-A-1-1 --off --output DisplayPort-1-3 --off --output DisplayPort-1-4 --off --output DisplayPort-1-5 --off
