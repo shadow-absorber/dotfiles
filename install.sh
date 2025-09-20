ece8f8d7990bd7946db7ca751eb1fd0e565003d4
shadow-absorber <sam.tunder@gmail.com>
2025-09-20 19:43:01 +0200

    add all packages that are must have for shadows config

--- install.sh
+++ install.sh
@@ -9,10 +9,7 @@ cd paru
 makepkg -si
 cd $HOME
 
-paru -S noto-fonts-emoji noto-fonts-cjk noto-fonts noto-fonts-extra discord element-desktop
-paru -S ttf-mononoki-nerd ghostty bat swaync lf yazi neovim rofi-wayland wofi zsh tmux variety wlogout
-paru -S swww
-paru -S wlsunset hyprlock hypridle
+paru -S bat beets btop catppuccin-cursors-mocha cava clamav cliphist darkly dhex dictd discord element-desktop eza fastfetch ffmpegthumbnailer fzf ghostty gnome-disk-utility grim grimblast-git hypridle hyprland hyprlock imagemagick keepassxc kitty less libqalculate librewolf-bin mangohud mpd mpd-discord-rpc mpd-mpris mpv mullvad-vpn-bin neovim noto-fonts-cjk noto-fonts-emoji noto-fonts-extra nsxiv nwg-look papirus-folders-catppuccin-git pavucontrol perl-image-exiftool pika-backup pkgfile plocate ploopy-headphones-toolbox-bin qmk qt5ct qt6ct rmpc rofi slurp steam swappy swaync swww tealdeer thunar thunar-archive-plugin thunar-media-tags-plugin thunderbird tmux ttf-dejavu ttf-dejavu-nerd ttf-liberation ttf-mononoki-nerd tumbler vial-appimage vim vlc vlc-plugins-all waybar wget wl-clipboard wlogout wlsunset wofi xarchiver xdg-desktop-portal-hyprland xdg-utils yazi zathura-cb zathura-djvu zathura-pdf-mupdf zathura-ps zoxide zsh zsh-autosuggestions zsh-history-substring-search zsh-syntax-highlighting zsh-you-should-use
 
 # installs packages to use stow for dotfiles
 paru -S git stow
