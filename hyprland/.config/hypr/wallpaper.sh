d459d17cc73babb5e8662d91ef90d8b53a1f72f0
shadow-absorber <sam.tunder@gmail.com>
2025-11-06 16:34:34 +0000

    update from swww to awww

--- hyprland/.config/hypr/wallpaper.sh
+++ hyprland/.config/hypr/wallpaper.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 while true; do
 	BG="$(find ~/Pictures/backgrounds/catppuccin/ -name '*.jpg' -o -name '*.png' | shuf -n1)"
-	PROGRAM="swww-daemon"
+	PROGRAM="awww-daemon"
 	trans_type="random"
 	if pgrep "$PROGRAM" >/dev/null; then
-		swww img "$BG" --transition-fps 120 --transition-type $trans_type --transition-duration 1
+		awww img "$BG" --transition-fps 120 --transition-type $trans_type --transition-duration 1
 	else
-		swww init && swww img "$BG" --transition-fps 120 --transition-type $trans_type --transition-duration 1
+		awww init && awww img "$BG" --transition-fps 120 --transition-type $trans_type --transition-duration 1
 	fi
 	sleep 300
 done
